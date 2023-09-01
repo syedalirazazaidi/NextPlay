9380d8498003d3934ee285266f96fd67bf241ff9
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-09-01 17:27:58 +0500

    updated step 05

--- step05_fetchingapi/fetchdata_on_server_from_api/app/page.tsx
+++ step05_fetchingapi/fetchdata_on_server_from_api/app/page.tsx
@@ -10,8 +10,10 @@ interface USERTYPE {
   updatedAt: string;
 }
 async function getData() {
-  const res = await fetch("https://api.escuelajs.co/api/v1/users");
- 
+  const res = await fetch(
+    "https://api.escuelajs.co/api/v1/users"
+  );
+
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -21,27 +23,27 @@ async function getData() {
 
 export default async function Home() {
   const users = await getData();
-  const renderData = users.slice(0, 6).map(({ name, email,avatar,creationAt }: USERTYPE) => {
-    return (
-      <div >
-      <div className='flex text-center  justify-center align-baseline'>
-        <Image
-      className='rounded-lg'
-        src={avatar}
-        alt="Picture of the user"
-        width={50}
-        height={50}
-        priority
-        
-      />
-        <p className='mt-2'>
-      - {name} - {email}
-        </p>
-    
-      </div>
-      </div>
-    );
-  });
+  const renderData = users
+    .slice(0, 6)
+    .map(({ name, email, avatar, creationAt,id }: USERTYPE) => {
+      return (
+        <div>
+          <div key={id} className="flex text-center  justify-center align-baseline">
+            <Image
+              className="rounded-lg"
+              src={avatar}
+              alt="Picture of the user"
+              width={50}
+              height={50}
+              priority
+            />
+            <p className="mt-2">
+              - {name} - {email}
+            </p>
+          </div>
+        </div>
+      );
+    });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {renderData}
