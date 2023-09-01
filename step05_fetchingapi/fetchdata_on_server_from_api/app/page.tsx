561ffc1e8196cee15abb277d95941aa826d27d8a
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-09-01 16:54:04 +0500

    add step05

---
+++ step05_fetchingapi/fetchdata_on_server_from_api/app/page.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+interface USERTYPE {
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  role: string;
+  avatar: string;
+  creationAt: string;
+  updatedAt: string;
+}
+async function getData() {
+  const res = await fetch("https://api.escuelajs.co/api/v1/users");
+ 
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const users = await getData();
+  const renderData = users.slice(0, 6).map(({ name, email,avatar,creationAt }: USERTYPE) => {
+    return (
+      <div >
+      <div className='flex text-center  justify-center align-baseline'>
+        <Image
+      className='rounded-lg'
+        src={avatar}
+        alt="Picture of the user"
+        width={50}
+        height={50}
+        priority
+        
+      />
+        <p className='mt-2'>
+      - {name} - {email}
+        </p>
+    
+      </div>
+      </div>
+    );
+  });
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {renderData}
+    </main>
+  );
+}
---
+++ step05_fetchingapi/fetchdata_on_server_from_api/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["picsum.photos","tmpfiles.org","img.freepik.com","api.lorem.space"],
+  },
+};
+module.exports = nextConfig;
