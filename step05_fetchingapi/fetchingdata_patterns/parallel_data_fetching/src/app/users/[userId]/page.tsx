73155a74ce6161f49717ba6bb1dbc06119ad7e6b
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-09-05 16:53:09 +0500

    add new data

--- step05_fetchingapi/fetchingdata_patterns/parallel_data_fetching/src/app/users/[userId]/page.tsx
+++ step05_fetchingapi/fetchingdata_patterns/parallel_data_fetching/src/app/users/[userId]/page.tsx
@@ -2,21 +2,21 @@ import getPhotos from "@/lib/getuserphotos";
 import getPosts from "@/lib/getuserpost";
 import Link from "next/link";
 import React from "react";
-interface POSTTypes {
-  userId: number;
-  title: string;
-  body: string;
-  it: number;
-}
-interface PHOTOTypes {
-  id: number;
-  avatar: string;
-}
+// interface POSTTypes {
+//   userId: number;
+//   title: string;
+//   body: string;
+//   it: number;
+// }
+// interface PHOTOTypes {
+//   id: number;
+//   avatar: string;
+// }
 export default async function page({ params }: { params: { userId: string } }) {
   const postsData =  getPosts(params.userId);
   const photosData =  getPhotos(params.userId);
   const [posts, photos] = await Promise.all([postsData, photosData])
-  console.log(posts.title)
+ 
   return (
     <div className="flex h-[200px] flex-col items-center justify-between mt-10" >
       <img src={photos.avatar} alt="myimg" width={60} height={60}></img>
